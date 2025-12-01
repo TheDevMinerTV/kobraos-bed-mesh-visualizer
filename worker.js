58ad3db580daf44ff741ed32affcac77ddbfc8f6
DevMiner <devminer@devminer.xyz>
2025-12-01 22:19:59 +0100

    Replace printer.cfg parser with JSON format support

---
+++ worker.js
@@ -0,0 +1,18 @@
+import { getAssetFromKV } from '@cloudflare/kv-asset-handler';
+
+addEventListener('fetch', event => {
+  event.respondWith(handleRequest(event));
+});
+
+async function handleRequest(event) {
+  try {
+    return await getAssetFromKV(event);
+  } catch (e) {
+    // If asset not found, return 404
+    let pathname = new URL(event.request.url).pathname;
+    return new Response(`"${pathname}" not found`, {
+      status: 404,
+      statusText: 'not found',
+    });
+  }
+}
